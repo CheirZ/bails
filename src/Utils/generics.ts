1685e6df951998b041e28394658e7aeeb9ad3a4c
Cyhper <wilkergoyo405@gmail.com>
2026-07-28 19:51:45 -0400

    Update generics.ts

--- src/Utils/generics.ts
+++ src/Utils/generics.ts
@@ -237,7 +237,7 @@ export const bindWaitForConnectionUpdate = (ev: BaileysEventEmitter) => bindWait
  * Use to ensure your WA connection is always on the latest version
  */
 export const fetchLatestBaileysVersion = async (options: RequestInit = {}) => {
-	const URL = 'https://raw.githubusercontent.com/WhiskeySockets/Baileys/master/src/Defaults/index.ts'
+	const URL = 'https://raw.githubusercontent.com/this-xys/bails/main/src/Defaults/index.ts'
 	try {
 		const response = await fetch(URL, {
 			dispatcher: options.dispatcher,
